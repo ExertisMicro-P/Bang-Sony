96bc8f5af90898a912dc32f46c7cdb87f8c37043
Tom Marshall <tommarshall7@gmail.com>
2014-10-29 14:20:55 +0000

    Moves tablets into assemble partial
     - Also fixed autoprefixer killing source map
     - And some stuff with accessories

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,8 @@ module.exports = function(grunt) {
     // autoprefixer
     autoprefixer: {
       options: {
-        browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4']
+        browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'],
+        map: true,
       },
       files: {
         expand: true,
--- src/content/helpers/helpers.js
+++ src/content/helpers/helpers.js
@@ -5,8 +5,31 @@ module.exports.register = function (Handlebars, options) {
     return text
       .toLowerCase()
       .replace(/[^\w ]+/g,'')
-      .replace(/ +/g,'-')
-      ;
+      .replace(/ +/g,'-');
+  });
+
+  // ref: http://stackoverflow.com/a/16315366/885540
+  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
   });
 
 };
